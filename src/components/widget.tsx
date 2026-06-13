57c8445e2f5c5575f0f7c7fe63e33446abf2c906
yasharsanaei <y.sanaei94@gmail.com>
2026-06-13 15:19:59 +0330

    feat: some styles for the widget

--- src/components/widget.tsx
+++ src/components/widget.tsx
@@ -7,7 +7,40 @@ type WidgetProps = {
     adData: AdData
 }
 
-export const Widget: Component<WidgetProps> = ({adData}) => {
-    if (!adData.content) return
-    return <div>This is a widget!</div>
+export const Widget: Component<WidgetProps> = ({ adData }) => {
+    const { content } = adData
+    if (!content) return
+    return (
+        <div class="mtr-container">
+            <a class="mtr-widget" href="#">
+                <div class="mtr-row">
+                    <div
+                        class="mtr-media"
+                        style={{
+                            'aspect-ratio':
+                                content.media.image.width /
+                                content.media.image.height,
+                        }}>
+                        <img
+                            src={content.media.image.url}
+                            alt={content.slogan}
+                        />
+                    </div>
+                    <div class="mtr-content">
+                        <p class="mtr-slogan">{content.slogan}</p>
+                        <div
+                            role="button"
+                            class="mtr-cta"
+                            style={{
+                                color: content.cta.color,
+                                'background-color': content.cta.backgroundColor,
+                                'border-radius': content.cta.borderRadius,
+                            }}>
+                            {content.cta.title}
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    )
 }
